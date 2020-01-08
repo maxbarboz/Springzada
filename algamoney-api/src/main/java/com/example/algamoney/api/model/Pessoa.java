8ff5da74ee3e61e22e936fd8bf5409a0548db53e
Maxsuel Storch <maxsuelstorch@gmail.com>
2020-01-08 15:31:25 -0300

    Desafio de criação das novas tabelas

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,64 @@
+package com.example.algamoney.api.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.internal.NotNull;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	private String nome;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@NotNull
+	private Boolean ativo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getName() {
+		return nome;
+	}
+
+	public void setName(String name) {
+		this.nome = name;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
