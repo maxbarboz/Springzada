1682b8bac3379a4072a6a48a28bde481c6dd8cd5
Maxsuel Storch <maxsuelstorch@gmail.com>
2020-01-08 16:14:21 -0300

    Adionado evento com spring no cadastro de nova pessoa/categoria

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResoucer.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResoucer.java
@@ -1,12 +1,13 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 
@@ -26,6 +27,9 @@ public class CategoriaResoucer {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll(); // fazendo select na tabela
@@ -35,6 +39,12 @@ public class CategoriaResoucer {
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
+//		this = representa que foi gerado para essa classe
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+
+		/*
 		// recuperação de recurso
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
 				.buildAndExpand( categoriaSalva.getCodigo()).toUri();
@@ -42,6 +52,7 @@ public class CategoriaResoucer {
 		
 		// retornando o valor da inserção na tabela
 		return ResponseEntity.created( uri ).body( categoriaSalva );
+		*/
 	}
 	
 	@GetMapping("/{codigo}")
