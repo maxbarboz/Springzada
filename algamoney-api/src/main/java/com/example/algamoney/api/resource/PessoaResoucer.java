01195b7d8e470318d9ea8eb5f310f4fcacd13d95
Maxsuel Storch <maxsuelstorch@gmail.com>
2020-01-09 10:23:41 -0300

    Adicionado a requisição de delete

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResoucer.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResoucer.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -43,5 +45,15 @@ public class PessoaResoucer {
 		 Pessoa pessoa = pessoaRepository.findById(codigo).get();
 		 return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT) // NO CONTENT SIGNIFICA -> BELEZA, FIZ OQUE TU QUERIA MAS NÃO TENHO NADA PARA RETORNAR
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.delete(pessoaRepository.getOne(codigo));
+	}
+	
+	
+	
+	
 	
 }
